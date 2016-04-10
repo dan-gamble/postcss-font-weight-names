f438afdd559b28ad7dd5db980c056f45b0f68291
Dan Gamble <dangamble89@gmail.com>
2016-04-10 13:53:20 +0100

    Initial writing of plugin & test

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 var postcss = require('postcss');
+var _ = require('lodash');
 
-module.exports = postcss.plugin('postcss-font-weight-names', function (opts) {
-    opts = opts || {};
+var nameMapping = {
+    100: ['thin', 'hairline'],
+    200: ['extra light', 'ultra light'],
+    300: ['light'],
+    400: ['normal', 'regular'],
+    500: ['medium'],
+    600: ['semi bold', 'demi bold'],
+    700: ['bold'],
+    800: ['extra bold', 'ultra bold'],
+    900: ['black', 'heavy']
+};
 
-    // Work with options here
+module.exports = postcss.plugin('postcss-font-weight-names', function () {
+    return function (css) {
+        var nameMappingValues = _.values(nameMapping);
+        // Flatten it so it's easier to check if it contains our prop's value
+        var nameMappingValuesFlattened = _.flatten(nameMappingValues);
 
-    return function (css, result) {
+        css.walkDecls('font-weight', function (decl) {
+            var value = _.split(decl.value.toLowerCase(), /[ -_]/).join(' ');
 
-        // Transform CSS AST here
+            if (_.includes(nameMappingValuesFlattened, value)) {
+                // The key which will be used as the new decl value
+                var matchedKey = null;
 
+                Object.keys(nameMapping).forEach(function (key) {
+                    if (_.includes(nameMapping[key], value)) {
+                        matchedKey = key;
+                    }
+                });
+
+                // Create the new font-weight value with a numeric value
+                decl.cloneBefore({ prop: 'font-weight', value: matchedKey });
+
+                // Remove the source declaration
+                decl.remove();
+            }
+        });
     };
 });
--- test.js
+++ test.js
@@ -11,10 +11,90 @@ function run(t, input, output, opts = { }) {
         });
 }
 
-/* Write tests here
-
-test('does something', t => {
-    return run(t, 'a{ }', 'a{ }', { });
+test('converts thin to 100', t => {
+    return run(t, 'a{ font-weight: thin; }', 'a{ font-weight: 100; }');
 });
 
-*/
+test('converts hairline to 100', t => {
+    return run(t, 'a{ font-weight: hairline; }', 'a{ font-weight: 100; }');
+});
+
+test('converts extra light to 200', t => {
+    return run(t, 'a{ font-weight: extra light; }', 'a{ font-weight: 200; }');
+});
+
+test('converts ultra light to 200', t => {
+    return run(t, 'a{ font-weight: ultra light; }', 'a{ font-weight: 200; }');
+});
+
+test('converts light to 300', t => {
+    return run(t, 'a{ font-weight: light; }', 'a{ font-weight: 300; }');
+});
+
+test('converts normal to 400', t => {
+    return run(t, 'a{ font-weight: normal; }', 'a{ font-weight: 400; }');
+});
+
+test('converts regular to 400', t => {
+    return run(t, 'a{ font-weight: regular; }', 'a{ font-weight: 400; }');
+});
+
+test('converts medium to 500', t => {
+    return run(t, 'a{ font-weight: medium; }', 'a{ font-weight: 500; }');
+});
+
+test('converts semi bold to 600', t => {
+    return run(t, 'a{ font-weight: semi bold; }', 'a{ font-weight: 600; }');
+});
+
+test('converts demi bold to 600', t => {
+    return run(t, 'a{ font-weight: demi bold; }', 'a{ font-weight: 600; }');
+});
+
+test('converts bold to 700', t => {
+    return run(t, 'a{ font-weight: bold; }', 'a{ font-weight: 700; }');
+});
+
+test('converts extra bold to 800', t => {
+    return run(t, 'a{ font-weight: extra bold; }', 'a{ font-weight: 800; }');
+});
+
+test('converts ultra bold to 800', t => {
+    return run(t, 'a{ font-weight: ultra bold; }', 'a{ font-weight: 800; }');
+});
+
+test('converts black to 900', t => {
+    return run(t, 'a{ font-weight: black; }', 'a{ font-weight: 900; }');
+});
+
+test('converts heavy to 900', t => {
+    return run(t, 'a{ font-weight: heavy; }', 'a{ font-weight: 900; }');
+});
+
+test('checks to make sure the case doesn\'t matter', t => {
+    return run(t,
+        'a{ font-weight: Heavy; font-weight: Semi Bold; }',
+        'a{ font-weight: 900; font-weight: 600; }'
+    );
+});
+
+test('handles hyphen separators', t => {
+    return run(t,
+        'a{ font-weight: extra-light; font-weight: semi-bold; }',
+        'a{ font-weight: 200; font-weight: 600; }'
+    );
+});
+
+test('handles underscore separators', t => {
+    return run(t,
+        'a{ font-weight: extra_light; font-weight: semi_bold; }',
+        'a{ font-weight: 200; font-weight: 600; }'
+    );
+});
+
+test('handles false values', t => {
+    return run(t,
+        'a{ font-weight: xtra light; font-weight: boldest; }',
+        'a{ font-weight: xtra light; font-weight: boldest; }'
+    );
+});
